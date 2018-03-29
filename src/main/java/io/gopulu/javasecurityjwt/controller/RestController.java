c2fbc017b9f1e003ccd8d71c6e0b5c6a05f6ac30
lingraj mahanand <lingraj.mahanand@aurea.com>
2018-03-29 07:50:41 +0530

    changes

--- src/main/java/io/gopulu/javasecurityjwt/controller/RestController.java
+++ src/main/java/io/gopulu/javasecurityjwt/controller/RestController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by lingrajmahanand on 3/28/18.
+ * This endpoint is secured with JWT tokens
+ * It is only accessible by the accounts with valid JWT tokens
  */
 @org.springframework.web.bind.annotation.RestController
 @RequestMapping("/app")
